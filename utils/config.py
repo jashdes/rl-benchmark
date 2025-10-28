1b246daa6efd883d88cd9914ca686949bdfd8e6c
Jash Desai <jashdesai27@gmail.com>
2025-10-28 17:05:56 -0500

    created config for experimental env

---
+++ utils/config.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, asdict
+
+
+@dataclass
+class ExperimentConfig:
+    """Configuration for a single experiment."""
+
+    algorithim: str
+    env_name: str
+    total_timesteps: int
+    n_eval_episodes: int
+    seed: int = 42
+    device: str = "auto"
+
+    # Algorithm-speicfic hyperparameters
+    learning_rate: float = 3e-4
+    n_steps: int = 2048  # For PPO
+    batch_size: int = 64
+
+
+def to_dict(self):
+    return asdict(self)
